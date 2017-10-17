7a5a84091ecc352ef276aab889a5e3a4d35f17d8
maho <maho@pagema.net>
2017-10-17 10:48:01 +0200

    - fear field and game system

---
+++ fear.py
@@ -0,0 +1,54 @@
+from kivy.factory import Factory
+from kivy.vector import Vector as V
+from kivent_core.systems.gamesystem import GameSystem
+
+
+class Fear(GameSystem):
+    PROBE_DIST = 10
+    PROBE_ANGLE_DIST = 45
+    MIN_GRADIENT = 0.1
+
+    def init_component(self, cindex, eid, zone, args):
+        super(Fear, self).init_component(cindex, eid, zone, args)
+        self.components[cindex].courage = 1.0
+
+    def entity(self, comp):
+        if comp is None:
+            return None
+        return self.gameworld.entities[comp.entity_id]
+
+    def update(self, _dt):
+        for c in self.components:
+            if c is None or c.role == 'danger':
+                continue
+            e = self.entity(c)
+
+            p = e.position.pos
+            points = [p] + [V((self.PROBE_DIST, 0)).rotate(x) + p
+                            for x in range(0, 360, self.PROBE_ANGLE_DIST)]  # noqa: E127
+
+            results = self.field_for_points(points)
+
+            best_point = min(results, key=results.get)
+            # gradient should be steep enough
+            if results[best_point] < results[p] - self.MIN_GRADIENT:
+                e.position.pos = best_point
+
+    def field_for_points(self, points):
+        # get dangers
+        dangers = []
+        for comp in self.components:
+            if comp and comp.role == 'danger':
+                dangers.append(self.entity(comp).position.pos)
+
+        ret = {}
+        for p in points:
+            vp = 0.0
+            for d in dangers:
+                dist = V(p).distance(d)
+                vp += 1000/dist
+            ret[tuple(p)] = vp
+        return ret
+
+
+Factory.register('Fear', cls=Fear)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from math import radians
+from math import radians, cos, sin
+from random import randint
 
 from kivy.app import App
 from kivy.factory import Factory
@@ -9,16 +10,20 @@ from kivy.vector import Vector
 from kivent_core.managers.resource_managers import texture_manager
 from kivent_core.systems.gamesystem import GameSystem
 
+import fear
+
 texture_manager.load_atlas('assets/objects.atlas')
 
 
 class SteeringSystem(GameSystem):
     queue_len = NumericProperty(10)
+    speed_threshold = NumericProperty(20)
 
     def __init__(self, *a, **kwa):
         super(SteeringSystem, self).__init__(*a, **kwa)
         self.queue = []
         self.angle = 0
+        self.speed = 0
 
     def on_touch_move(self, touch):
         self.queue.append(touch.pos)
@@ -27,30 +32,39 @@ class SteeringSystem(GameSystem):
         vec = Vector(self.queue[-1]) - self.queue[0]
         self.angle = vec.angle((0, 100))
 
-    def update(self, _dt):
+        self.apply_angle()
+
+        if vec.length2() > (self.speed_threshold**2):
+            self.speed = 5
+
+    def apply_angle(self):
         for comp in self.components:
             eid = comp.entity_id
 
             e = self.gameworld.entities[eid]
             e.rotate.r = radians(self.angle)
 
+    def apply_run(self, e):
+        vx, vy = - self.speed * sin(radians(self.angle)), self.speed * cos(radians(self.angle))
+        x, y = e.position.pos
+        e.position.pos = (x + vx, y + vy)
+
+    def update(self, _dt):
+        for comp in self.components:
+            eid = comp.entity_id
+            e = self.gameworld.entities[eid]
+            self.apply_run(e)
+        self.speed /= 2
+
 
 Factory.register('SteeringSystem', cls=SteeringSystem)
-
-
-class VelSystem(GameSystem):
-    def update(self, dt):
-        pass
-
-
-Factory.register('VelSystem', cls=VelSystem)
 
 
 class SneakGame(Widget):
     def __init__(self, **kwargs):
         super(SneakGame, self).__init__(**kwargs)
         self.gameworld.init_gameworld(
-            ['renderer', 'rotate', 'position', 'vel'],
+            ['renderer', 'rotate', 'position', 'steering', 'fear'],
             callback=self.init_game)
 
     def init_game(self):
@@ -63,15 +77,16 @@ class SneakGame(Widget):
 
     def setup_states(self):
         self.gameworld.add_state(state_name='main',
-                                 systems_added=['renderer', 'vel'],
+                                 systems_added=['renderer'],
                                  systems_removed=[], systems_paused=[],
-                                 systems_unpaused=['renderer', 'vel'],
+                                 systems_unpaused=['renderer'],
                                  screenmanager_screen='main')
 
     def set_state(self):
         self.gameworld.state = 'main'
 
     def draw_some_stuff(self):
+        # draw person
         self.gameworld.init_entity({
                 'renderer': {
                     'texture': 'person',
@@ -81,8 +96,20 @@ class SneakGame(Widget):
                 'rotate': 0,
                 'steering': {},
                 'position': (100, 100),
-                'vel': {'vx': 10, 'vy': 10}
-            }, ['position', 'vel', 'rotate', 'renderer', 'steering'])
+                'fear': {'role': 'danger'}
+            }, ['position', 'rotate', 'renderer', 'steering', 'fear'])
+
+        # draw rats
+        mapw, maph = self.gamemap.size
+        for _x in range(20):
+            self.gameworld.init_entity({
+                'renderer': {'texture': 'rat',
+                             'size':  (20, 20),
+                             'render': True},
+                'rotate': 0,
+                'fear': {'role': 'victim'},
+                'position': (randint(0, mapw), randint(0, maph))},
+                ['position', 'rotate', 'renderer', 'fear'])
 
 
 class SneakApp(App):
